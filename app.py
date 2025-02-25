be08d20c171d31c5bd50b28e5f7625e26096cc28
Mario Rodarte <mariohrodarte@outlook.com>
2025-02-24 18:46:37 -0600

    Segundo commit

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import mysql.connector
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    conn = mysql.connector.connect(
+        host = '',
+        user = 'root',
+        password = '',
+        database = 'db'
+    )
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM students')
+    students = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', students = students)
 
 
 if __name__ == '__main__':
